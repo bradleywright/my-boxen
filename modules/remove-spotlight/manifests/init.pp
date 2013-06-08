5e007295d975dba438b68d12ffb29ca676af6414
Bradley Wright <brad@intranation.com>
2013-06-08 22:36:30 +0100

    Refactor remove-spotlight

--- modules/remove-spotlight/manifests/init.pp
+++ modules/remove-spotlight/manifests/init.pp
@@ -1,7 +1,12 @@
 # Removes Spotlight search
 class remove-spotlight {
   exec { 'mv-spotlight':
-    command => 'sudo mv /System/Library/CoreServices/Search.bundle /System/Library/CoreServices/Search.bundle.bak && killall SystemUIServer',
-    unless  => 'test ! -e /System/Library/CoreServices/Search.bundle',
+    command => 'sudo mv /System/Library/CoreServices/Search.bundle /System/Library/CoreServices/Search.bundle.bak',
+    notify  => Exec['killall SystemUIServer'],
+    unless  => 'test ! -e /System/Library/CoreServices/Search.bundle';
+  }
+
+  exec { 'killall SystemUIServer':
+    refreshonly => true;
   }
 }
