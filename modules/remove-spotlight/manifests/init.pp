316d9d84bcbe699a447cd8e79d0350af2ac13898
Bradley Wright <brad@intranation.com>
2014-01-21 22:42:07 +0000

    Make Spotlight remover smarter

--- modules/remove-spotlight/manifests/init.pp
+++ modules/remove-spotlight/manifests/init.pp
@@ -1,7 +1,7 @@
 # Removes Spotlight search
 class remove-spotlight {
   exec { 'mv-spotlight':
-    command => 'sudo mv /System/Library/CoreServices/Search.bundle /System/Library/CoreServices/Search.bundle.bak',
+    command => 'sudo mv -f /System/Library/CoreServices/Search.bundle /System/Library/CoreServices/Search.bundle.bak',
     notify  => Exec['killall SystemUIServer'],
     unless  => 'test ! -e /System/Library/CoreServices/Search.bundle';
   }
