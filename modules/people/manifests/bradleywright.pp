1095721b9f815d151336fd19dcae97c5feb0ffef
Bradley Wright <brad@intranation.com>
2014-05-07 23:09:49 +0100

    Just use Emacs directly
    We don't really need the module anymore - I can call the relevant bits
    straight from my own manifest.

--- modules/people/manifests/bradleywright.pp
+++ modules/people/manifests/bradleywright.pp
@@ -135,4 +135,27 @@ unalias git",
 
   keyremap4macbook::remap{ 'space_cadet.force_correct_shifts': }
 
+  # Emacs
+  package { 'Emacs':
+    provider => 'appdmg',
+    source   => 'http://emacsformacosx.com/emacs-builds/Emacs-pretest-24.3.90-universal-10.6.8.dmg',
+    notify   => Exec['fix-emacs-termcap'],
+  }
+
+  # So ansi-term behaves itself: http://stackoverflow.com/a/8920373
+  exec { 'fix-emacs-termcap':
+    command     => 'tic -o \
+      ~/.terminfo \
+      /Applications/Emacs.app/Contents/Resources/etc/e/eterm-color.ti',
+    provider    => shell,
+    refreshonly => true,
+  }
+
+  file { "${boxen::config::envdir}/emacs-macosx.sh":
+    content => "export PATH=/Applications/Emacs.app/Contents/MacOS/bin:\$PATH
+alias emacs=/Applications/Emacs.app/Contents/MacOS/Emacs-10.7
+",
+    require => File[$boxen::config::envdir],
+  }
+
 }
