f1b34367548bf16f04c4406c1ba28c93608ea64f
Bradley Wright <brad@intranation.com>
2014-11-14 20:18:13 +0000

    Make sure we copy GOPATH

--- modules/people/manifests/bradleywright.pp
+++ modules/people/manifests/bradleywright.pp
@@ -68,7 +68,7 @@ class people::bradleywright {
 
   file { "${my_home}/.emacs.d/local/${::hostname}.el":
     mode    => '0644',
-    content => "(exec-path-from-shell-copy-envs '(\"BOXEN_NVM_DIR\" \"BOXEN_NVM_DEFAULT_VERSION\"))
+    content => "(exec-path-from-shell-copy-envs '(\"BOXEN_NVM_DIR\" \"BOXEN_NVM_DEFAULT_VERSION\" \"GOPATH\"))
 ",
     require => Repository[$emacs],
   }
