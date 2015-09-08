4ed805e759b1dc7eff96718e43bc2f943c386ff1
Bradley Wright <brad@intranation.com>
2015-09-08 23:11:22 +0100

    Boxen creates its own symlink

--- modules/people/manifests/bradwright.pp
+++ modules/people/manifests/bradwright.pp
@@ -3,11 +3,6 @@ class people::bradwright {
 
   $my_home  = "/Users/${::boxen_user}"
 
-  file { "${::boxen_srcdir}/boxen":
-    ensure => 'link',
-    target => "${::boxen_home}/repo"
-  }
-
   $dotfiles = "${::boxen_srcdir}/dotfiles"
 
   repository { $dotfiles:
