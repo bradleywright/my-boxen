e5286afd6c6d0c11a5beff3af0fce4ba7bf95029
Bradley Wright <brad@intranation.com>
2015-10-19 22:29:07 +0100

    Fix PATH issues in ZSH
    El Capitan has broken the PATH in ZSH, more info here:
    
    http://www.zsh.org/mla/users/2015/msg00725.html

--- modules/people/manifests/bradwright.pp
+++ modules/people/manifests/bradwright.pp
@@ -38,12 +38,16 @@ class people::bradwright {
   file { "${my_home}/.local_zshrc":
     mode    => '0644',
     content => "cdpath=(${::boxen_srcdir} ~)
+
+setopt global_rcs
 "
   }
 
   file { "${my_home}/.local_zshenv":
     mode    => '0644',
-    content => "[[ -f ${boxen::config::boxen_home}/env.sh ]] && . ${boxen::config::boxen_home}/env.sh
+    content => "setopt no_global_rcs
+
+[[ -f ${boxen::config::boxen_home}/env.sh ]] && . ${boxen::config::boxen_home}/env.sh
 "
   }
 
