ff65d8c3a74ed358b624472342922f6e8e589d4a
Bradley Wright <brad@intranation.com>
2015-10-29 22:34:48 +0000

    Fix El Capitan PATH issues properly

--- modules/people/manifests/bradwright.pp
+++ modules/people/manifests/bradwright.pp
@@ -1,6 +1,12 @@
 class people::bradwright {
   include zsh
 
+  # Null out El Capitan's zprofile so it doesn't break my path:
+  # http://www.zsh.org/mla/users/2015/msg00724.html
+  file { '/etc/zprofile':
+    content => ''
+  }
+
   class { 'osx::global::key_repeat_delay':
     delay => 200
   }
